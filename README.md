023c3392a22363f7ff6024e1f84fca636b460282
vijay <vj.khambe@gmail.com>
2021-03-29 23:34:58 +0530

    change version and added k8s commands

--- README.md
+++ README.md
@@ -16,3 +16,32 @@ sudo docker build -f Dockerfile-package-only -t vjkhambe/pkg-myspring-boot:1.0.0
 sudo docker run -p88:8080 vjkhambe/pkg-myspring-boot:1.0.0
 
 sudo docker image push docker.io/vjkhambe/myspring-boot:1.0.0
+
+
+
+kubectl delete -n default deployment myspring-boot-minikube
+
+kubectl create deployment myspring-boot-minikube --image=docker.io/vjkhambe/myspring-boot:1.0.0
+
+kubectl expose deployment myspring-boot-minikube --type=NodePort --port=8080
+
+kubectl get services myspring-boot-minikube
+
+minikube service myspring-boot-minikube
+
+kubectl port-forward service/myspring-boot-minikube 7080:8080
+
+
+## load balanced deployment of myspring-boot app
+
+
+kubectl create deployment balanced-myspring-boot-minikube --image=docker.io/vjkhambe/myspring-boot:1.0.0
+
+kubectl expose deployment balanced-myspring-boot-minikube --type=LoadBalancer --port=8080
+
+kubectl get services balanced-myspring-boot-minikube
+
+minikube service balanced-myspring-boot-minikube
+
+kubectl port-forward service/balanced-myspring-boot-minikube 7081:8080
+
--- src/main/java/com/vjkhambe/springapp/HelloController.java
+++ src/main/java/com/vjkhambe/springapp/HelloController.java
@@ -1,24 +1,20 @@
 package com.vjkhambe.springapp;
 
-import org.springframework.web.bind.annotation.RequestMapping;  
-import org.springframework.web.bind.annotation.RestController;  
-
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
-public class HelloController
-{
-	@RequestMapping("/hello")
-	public String hello()
-	{
-		return "Hello World";
-	}
-	public static void main(String[] args) throws Exception {
-        	SpringApplication.run(HelloController.class, args);
-    	}
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World - Docker";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloController.class, args);
+    }
 }
 
